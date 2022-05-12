d418ace89b83217ca0254c4faa9b2247d3d6bb46
podmar <marta.podziewska@gmail.com>
2022-05-12 16:39:41 +0200

    removed redundant comments

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,51 +2,7 @@ import React from 'react'
 import { Button, Card } from 'react-bootstrap'
 
 function CharacterCard(data) {
-
-  // return (
-  //   <div className='CardClass'>
-  //     <Card style={{ width: '10rem' }}>
-  //       <Card.Img variant="top" src={data.image} />
-  //       <Card.Body>
-  //         <Card.Title>{data.name}</Card.Title>
-  //         <Card.Text>
-  //           <p>{`${data.species} ${data.gender}`}</p>
-  //         </Card.Text>
-  //         <Button variant="primary">More info</Button>
-  //       </Card.Body>
-  //     </Card>
-  //   </div>
-  // )
-
-  // return (
-  //   <div className='FlipCard'>
-
-  //     <div className='FlipCardInner'>
-
-  //       <div className='FlipCardFront'>
-  //         <Card style={{ width: '10rem' }}>
-  //         <Card.Img variant="top" src={data.image} />
-  //         <Card.Body>
-  //           <Card.Title>{data.name}</Card.Title>
-  //         </Card.Body>
-  //         </Card>
-  //       </div>
-
-  //       <div className='FlipCardBack'>
-  //         <Card style={{ width: '10rem' }}>
-  //           <Card.Body>
-  //             <Card.Title>{data.name}</Card.Title>
-  //             <Card.Text>
-  //               <p>{`${data.species} ${data.gender}`}</p>
-  //             </Card.Text>
-  //             <Button variant="primary">More info</Button>
-  //           </Card.Body>
-  //         </Card>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
-
+  
   return (
     <div className='FlipCard'>
 
