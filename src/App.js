e4ad7eaa50aabd30821aeb5eb4dd6b3aa56bd016
podmar <marta.podziewska@gmail.com>
2022-05-12 14:16:08 +0200

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { Container, Navbar, Row } from 'react-bootstrap';
 import './App.css';
 import Characters from "./Characters.js"
 
 function App() {
   return (
     <div className="App">
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Rick and Morty Character List</Navbar.Brand>
+          </Container>
+        </Navbar>
+      <Container>
+      
+        <Row>
+
+        </Row>
     
-      <p>This is the main App component.</p>
-      <Characters/> 
+        <Row>
+          {/* <p>This is the main App component.</p> */}
+          <Characters /> 
+        </Row>
+
+        <Row></Row>
+        
+      </Container>
 
     </div>
   );
